9416904c3d3d0d120334f0000c7c7087ea8a87b2
Kapend Tshek <61226238+Kapend-Tshek@users.noreply.github.com>
2023-08-27 15:42:09 +0200

    Add files via upload

---
+++ cpi-main/cpi/prototype.h
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <dirent.h>
+#include <string.h>
+#include <sys/stat.h>
+
+
+
+// structure packages
+
+typedef struct package{
+    char *name;         // nom de la structure
+    char *path;         // adresse de la structure 
+}package;
+
+
+
+// fonction utils
+int cheminCourant();
+
+int copier(char *source, char *destination);
+int recherchePackage(char *path, char *namePackage);
+int supprimer(char *path, char *namePackage);
+
+
+
+
+// fonction argument de ligne de commande
+int install(char *path, char *namePackage);
+    // prendre le dossier qui a le nom 'namepackage' le copier dans path
+int uninstall(char *path, char *namePackage);
+    // aller dans path/packages chercher le dossier namepackage supprime
+int list(char *path);
